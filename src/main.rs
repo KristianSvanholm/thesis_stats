549cb5cfb6727bff7ac5ce08be3e3cce2b5ab09f
KristianSvanholm <KristianRorenSvanholm@gmail.com>
2025-03-23 19:33:26 +0100

    Add kendalls Tau calculator

---
+++ src/main.rs
@@ -0,0 +1,67 @@
+use std::cmp::Ordering;
+use std::fs;
+use std::collections::HashMap;
+
+fn main() {
+
+    let chips = ["i78700", "n150", "epyc1", "epyc2", "m1pro", "m2pro", "m3max"];
+
+    let mut collect = HashMap::new();
+    for chip in chips {
+        let ranks: Vec<String> = fs::read_to_string(format!("data/{}/ranks.txt",chip))
+            .expect("Should be able to read this file")
+            .split("\n")
+            .skip(1) // Skip title
+            .filter(|p| !p.is_empty())
+            .map(|entry| 
+                entry.split(" ")
+                    .filter(|p| !p.is_empty())
+                    .collect::<Vec<_>>()[1]
+                    .to_string()
+            )
+            .collect();
+
+        collect.insert(chip, ranks);
+    }
+
+    let keys: Vec<&str>= collect.keys().cloned().collect();
+
+    for n in 0..keys.len() {
+        for m in n+1..keys.len() {
+            let a = keys[n];
+            let b = keys[m];
+            let tau = kendall(collect[a].clone(), collect[b].clone());
+            println!("{:7} - {:7} | Tau: {:.3}", a,b,tau);
+        }
+    }
+
+}
+
+fn kendall<T: PartialEq>(a: Vec<T>, b: Vec<T> ) -> f32 {
+    let mut ix = vec![];
+    for elem in a {
+        let i = b.iter().position(|x| *x == elem).unwrap();
+        ix.push(i);
+    }
+
+
+    let mut con:f32 = 0.0;
+    let mut dis:f32 = 0.0;
+    for n in 0..=ix.len()-1 {
+        let v = ix[n];
+        for m in n+1..=ix.len()-1 {
+
+            match v.cmp(&ix[m]){ 
+                Ordering::Less =>  con +=1.0,
+                Ordering::Greater => dis +=1.0,
+                _ =>  break,
+            };
+
+        }
+    }
+
+    //println!("{} - {}", con, dis);
+
+    let n = ix.len() as f32;
+    (con-dis) / ((n*(n-1.0))/2.0)
+}
