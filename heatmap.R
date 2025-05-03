14fe863fa9bad66ab446bf099df9c71075ba3bf0
kristian <kristianrorensvanholm@gmail.com>
2025-05-03 11:11:29 +0200

    kendalls heatmap adjusted

--- heatmap.R
+++ heatmap.R
@@ -9,9 +9,12 @@ tau <- read.csv("data/kendalls_tau.csv", header = TRUE, sep = ",")
 
 print(tau)
 
-heatmap <- ggplot(tau, aes(x=A, y=B, fill=Value)) + geom_tile() +theme_bw()+
-    geom_text(data=tau, aes(x=A, y=B, label=Value, color=ifelse(Value < 0.94, 1, 0))) +
-    labs(x="", y="") +
+heatmap <- ggplot(tau, aes(x=A, y=B, fill=Value)) + geom_tile() +theme_bw()+ coord_equal() +
+    geom_text(data=tau, aes(x=A, y=B, label=Value, angle=45, color=ifelse(Value < 0.94, 1, 0))) +
+    scale_x_discrete( guide=guide_axis(angle = 45), position = "top") +
+    scale_y_discrete( guide=guide_axis(angle = 45)) +
+    labs(x="", y="") + theme(panel.border = element_blank(), panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))+
     theme(legend.position = "none")
 
 ggsave(file= "heatmap.svg",plot=heatmap)
